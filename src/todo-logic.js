61dc6c418ff3409c30dbf07cc10f18f05f316307
subarnapkrl <subarnapokharel252@gmail.com>
2023-08-04 14:51:04 +0545

    TodoList-side-logic

---
+++ src/todo-logic.js
@@ -0,0 +1,11 @@
+const toDo = (title, description, dueDate, priority) => {
+  let status = false;
+
+  return { title, description, dueDate, priority, status };
+};
+
+const toggleStatus = (status) => {
+  return !status;
+};
+
+export { toDo, toggleStatus };
