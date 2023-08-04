a93111df77cbe2ddbd152ab96c12358e852215dd
subarnapkrl <subarnapokharel252@gmail.com>
2023-08-04 14:50:11 +0545

    Logic-of Project-section

---
+++ src/project-logic.js
@@ -0,0 +1,15 @@
+const Project = (name) => {
+  let todos = [];
+
+  return { name, todos };
+};
+
+const deleteToDo = (todosArray, removedTodoIndex) => {
+  todosArray.splice(removedTodoIndex, 1);
+};
+
+const deleteProject = (projectsList, deletedProjectIndex) => {
+  projectsList.splice(deletedProjectIndex, 1);
+};
+
+export { Project, deleteToDo, deleteProject };
